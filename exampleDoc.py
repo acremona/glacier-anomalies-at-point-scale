5241293adc1b98fe8639a7bb3e0d2471c10d068f
joelborner <bornerj@student.ethz.ch>
2020-09-30 17:56:57 +0200

    adding a sample python file with some documentation tests

---
+++ exampleDoc.py
@@ -0,0 +1,33 @@
+def sampleFunction(arg1, arg2):
+    """
+    This is just a test function taking 2 arguments, sets the first equal the second and returns this argument.
+
+    :param arg1: Some random value
+    :param arg2: Another random value
+    :return: arg1
+
+    """
+    arg1 = arg2
+    return arg2
+
+
+def anotherSampleFunction(arg1, arg2):
+    """
+    This is another test function, adding both input arguments and returning the sum. Testing the google docstring format
+    Args:
+        arg1: Some random value
+        arg2: Some random value
+
+    Returns: arg1 + arg2
+
+    """
+    return arg1+arg2
+
+
+def thirdSampleFunction():
+    """
+    Another sample function, not taking any arguments and not returning anything. Literally not doing anything :)
+    :return: Nothing
+    """
+    return None
+
