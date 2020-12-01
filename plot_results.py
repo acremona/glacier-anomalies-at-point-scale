4dfed0191eb4785889d03e150ac7a793730c6572
acremona <acremona@student.ethz.ch>
2020-12-01 14:46:48 +0100

    Changes in plot results

--- plot_results.py
+++ plot_results.py
@@ -17,8 +17,15 @@ import pylab
 
 max_val_idx = 82
 
+#datetime vector
+start_date = datetime.date(2019, 6, 28)
+start_min = datetime.datetime(2019, 6, 27, 11, 59)
+temp_date = start_date
+delta_day = datetime.timedelta(days=1)
+
+
 # import data from excel
-data = pd.read_excel("C:\\Users\\User\\Desktop\\Eth\\MasterIII\\Project\\1008.xlsx",sheet_name='Final')
+data = pd.read_excel("C:\\Users\\User\\Desktop\\Eth\\MasterIII\\Project\\1001.xlsx",sheet_name='Final')
 time_val = data['time val'].tolist()
 time_val = time_val[0:max_val_idx]
 rate_val = data['rate val'].tolist()
@@ -26,20 +33,13 @@ dy_cum_val = data['dy cum val'].tolist()
 dy_cum_val = dy_cum_val[0:max_val_idx]
 time = data['time']
 dy_cum1 = data['dy cum1']
-dy_cum2 = data['dy cum2']
+#dy_cum2 = data['dy cum2']
 dy_cum3 = data['dy cum3']
 dy_mean = data['mean']
 std1 = data[' std dev cum1'].tolist()
-std2 = data[' std dev cum2'].tolist()
+#std2 = data[' std dev cum2'].tolist()
 std3 = data[' std dev cum3'].tolist()
-
-#datetime vector
-start_date = datetime.date(2019, 6, 28)
-start_min = datetime.datetime(2019, 6, 27, 15, 30)
-temp_date = start_date
-end_date = datetime.date(2019, 9, 17)
-delta_day = datetime.timedelta(days=1)
-
+stdmean = data['mean std']
 
 dates = []
 dates_min = []
@@ -54,15 +54,16 @@ for t1 in time:
 
 #plot
 fig, ax = plt.subplots()
+plt.figure(figsize=(8,4))
 plt.plot(dates, dy_cum_val, '.', color= 'black', label='Validation data')
 plt.plot(dates_min,dy_cum1,'-', color='blue', label='Template 1')
-plt.plot(dates_min,dy_cum2,'-', color='purple', label='Template 2')
+#plt.plot(dates_min,dy_cum2,'-', color='purple', label='Template 2')
 plt.plot(dates_min,dy_cum3,'-', color='limegreen', label='Template 3')
 plt.plot(dates_min,dy_mean,'-', color='red', label='Mean',linewidth=2)
 #plt.fill_between(dates_min,dy_cum1-std1, dy_cum1+std1,color='blue', alpha=0.2)
 #plt.fill_between(dates_min,dy_cum2-std2, dy_cum2+std2,color='orange', alpha=0.2)
 #plt.fill_between(dates_min,dy_cum3-std3, dy_cum3+std3,color='green', alpha=0.2)
-plt.fill_between(dates_min,dy_mean-std3, dy_mean+std3,color='red', alpha=0.15)
+plt.fill_between(dates_min,dy_mean-stdmean, dy_mean+stdmean,color='red', alpha=0.15)
 plt.gcf().autofmt_xdate()
 
 # Format the date into months & days
@@ -79,6 +80,7 @@ plt.ylabel('Cumulative displacement [m]',fontsize='large',fontweight= 'heavy')
 
 plt.legend(loc='upper left', shadow=False, fontsize='medium')
 plt.grid(color='gray', linestyle=':', linewidth=0.5)
+plt.savefig('1001.png')
 plt.show()
 
 
